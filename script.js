0c9fec9e883040f69efcb0433e4c86462f234b7d
Deepak Yadav <139697406+DGyadav123@users.noreply.github.com>
2024-02-04 08:27:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Spotify");
+let songs = [
+    {songName:"Salam-e-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"Salam-e-Ishq", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+]
+let audioElement = new Audio();
